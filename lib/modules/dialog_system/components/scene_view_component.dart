e1b6df0521c55a8c5ecc58a2ff7d6eef49401ecc
4dominic888 <4dominin888@gmail.com>
2024-04-22 17:11:06 -0500

    uso inicial de comandos de yarn y etiquetas

--- lib/modules/dialog_system/components/scene_view_component.dart
+++ lib/modules/dialog_system/components/scene_view_component.dart
@@ -3,12 +3,11 @@ import 'dart:async';
 import 'package:a_new_begin_again_vn/modules/dialog_system/components/text_container.dart';
 import 'package:a_new_begin_again_vn/modules/dialog_system/screens/screen_dialog.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flame/input.dart';
 import 'package:jenny/jenny.dart';
 
 class SceneViewComponent extends PositionComponent with DialogueView, HasWorldReference<ScreenDialog>{
-  late SpriteComponent bg;
-  late SpriteComponent character1;
   late SpriteComponent boxTextContainer;
   late final ButtonComponent forwardNextButtonComponent;
   Completer<void> _forwardCompleter = Completer();
@@ -17,14 +16,8 @@ class SceneViewComponent extends PositionComponent with DialogueView, HasWorldRe
 
   @override
   FutureOr<void> onLoad() {
-    bg = SpriteComponent(sprite: world.bg);
-    character1 = SpriteComponent(sprite: world.character1)
-      ..size /=2
-      ..position.y = -38;
-
     boxTextContainer = SpriteComponent(sprite: world.boxTextContainer)
       ..position.y = 285;
-
 
     textContainer = DialoguePerCharText(
       text: '',
@@ -50,11 +43,17 @@ class SceneViewComponent extends PositionComponent with DialogueView, HasWorldRe
         }
     });
 
-    addAll([bg, character1, boxTextContainer, forwardNextButtonComponent, textContainer]);
+    addAll([forwardNextButtonComponent, boxTextContainer..priority = 2, textContainer..priority = 4]);
 
     return super.onLoad();
   }
 
+  @override
+  FutureOr<void> onNodeStart(Node node) async {
+    final bg = SpriteComponent(key: ComponentKey.named('bg'), sprite: await world.gameRef.loadSprite('bg/${node.tags["initBg"]}'));
+    add(bg..priority = -1);
+    return super.onNodeStart(node);
+  }
 
   @override
   FutureOr<bool> onLineStart(DialogueLine line) async {
@@ -65,6 +64,14 @@ class SceneViewComponent extends PositionComponent with DialogueView, HasWorldRe
   }
 
   Future<void> _advance(DialogueLine line) async{
+
+    if(line.tags.contains("#scream")){
+      final shake = MoveEffect.by(Vector2(0,-3), InfiniteEffectController(ZigzagEffectController(period: 0.2)));
+      final chSpr = children.query<SpriteComponent>().firstWhere((element) => element.key == ComponentKey.named('ch_${line.character?.name}'));
+      chSpr.add(shake);
+      Future.delayed(const Duration(seconds: 3)).then((value) => shake.pause());
+    }
+
     final characterName = line.character?.name ?? '';
     final dialogueLine = '$characterName: ${line.text}';
     remove(textContainer);
--- lib/modules/dialog_system/screens/screen_dialog.dart
+++ lib/modules/dialog_system/screens/screen_dialog.dart
@@ -9,8 +9,6 @@ import 'package:jenny/jenny.dart';
 
 class ScreenDialog extends World with HasGameRef<VisualNovel>{
 
-  late final Sprite bg;
-  late final Sprite character1;
   late final Sprite boxTextContainer;
   YarnProject yarnProject = YarnProject();
   late final SceneViewComponent sceneViewComponent;
@@ -18,24 +16,37 @@ class ScreenDialog extends World with HasGameRef<VisualNovel>{
   @override
   FutureOr<void> onLoad() async {
 
-    final _fader = FadeComponent(
+    final fader = FadeComponent(
       size: gameRef.cam.viewport.virtualSize,
       color: Colors.black,
       time: 1.7
     );
-    gameRef.cam.viewport.add(_fader);
-    _fader.outEffect(() { });    
+    gameRef.cam.viewport.add(fader);
+    fader.outEffect(() {});
 
-    bg = await gameRef.loadSprite('bg/city-bg.jpg');
-    character1 = await gameRef.loadSprite('characters/character_A.png');
     boxTextContainer = await gameRef.loadSprite('dialogs/default_dialog_container.png');
     sceneViewComponent = SceneViewComponent();
     String startDialog = await rootBundle.loadString('assets/yarn/start.yarn');
 
+    yarnProject.commands.addCommand4("characterAppear", (String chName, String fileName, String ext, String pos) async {
+      final SpriteComponent character1 = SpriteComponent(
+        key: ComponentKey.named('ch_$chName'),
+        sprite: await gameRef.loadSprite('characters/$fileName.$ext')
+      );
+      character1.size /= character1.size.y / gameRef.size.y;
+      switch (pos) {
+        case "center": character1.position.x = (gameRef.size.x *0.5) - 160; break;
+        case "left": character1.position.x = (gameRef.size.x *0.25) - 160; break;
+        case "right": character1.position.x = (gameRef.size.x *0.75) - 160; break;
+      }
+
+      sceneViewComponent.add(character1);
+      character1.priority = 0;
+    });
 
     yarnProject.parse(startDialog);
     var dialogueRunner = DialogueRunner(yarnProject: yarnProject, dialogueViews: [sceneViewComponent]);
-    dialogueRunner.startDialogue('Test');
+    dialogueRunner.startDialogue('mini_historia_fulbo');
 
     add(sceneViewComponent);
 
